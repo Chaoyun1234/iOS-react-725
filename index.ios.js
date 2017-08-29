56eed8a822f039094a26054e5bd844dda425f860
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-08-29 16:49:12 +0800

    AddPush

--- index.ios.js
+++ index.ios.js
@@ -8,13 +8,16 @@ import React, { Component } from 'react';
 import MobileCenter from 'mobile-center';
 import Crashes from "mobile-center-crashes";
 import Analytics from "mobile-center-analytics";
+import Push from "mobile-center-push";
+import { AppState, Alert } from 'react-native';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
   Button,
-  Alert
+  Alert,
+  AppState
 } from 'react-native';
 async function ClickBtn() {  
   const installId = await MobileCenter.getInstallId();
@@ -56,7 +59,34 @@ export default class iosreact725 extends Component {
     );
   }
 }
+Push.setEventListener({
+  pushNotificationReceived: function (pushNotification) {
+    let message = pushNotification.message;
+    let title = pushNotification.title;
 
+    if (message === null || message === undefined) {
+      // Android messages received in the background don't include a message. On Android, that fact can be used to
+      // check if the message was received in the background or foreground. For iOS the message is always present.
+      title = "Android background";
+      message = "<empty>";
+    }
+
+    // Custom name/value pairs set in the Mobile Center web portal are in customProperties
+    if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+      message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+    }
+
+    if (AppState.currentState === 'active') {
+      Alert.alert(title, message);
+    }
+    else {
+      // Sometimes the push callback is received shortly before the app is fully active in the foreground.
+      // In this case you'll want to save off the notification info and wait until the app is fully shown
+      // in the foreground before displaying any UI. You could use AppState.addEventListener to be notified
+      // when the app is fully in the foreground.
+    }
+  }
+});
 const styles = StyleSheet.create({
   container: {
     flex: 1,
